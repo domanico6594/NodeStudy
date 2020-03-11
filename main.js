052e5b147dd1eeba60bde869cb28a386e29c292d
성현 <mingyov0807@gmail.com>
2020-03-11 15:15:17 +0900

    콜백 개념 정리

--- main.js
+++ main.js
@@ -6,13 +6,13 @@ function templateHTML(title, list, body) {
 	return `<!doctype html>
 		<html>
 		<head>
-			<title>WEB1 - ${title}</title>
+			<title>WEB2! - ${title}</title>
 			<meta charset="utf-8">
 			<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 		</head>
 		
 		<body>
-			<h1><a href="/">WEB</a></h1>
+			<h1><a href="/">WEB2</a></h1>
 			${list}
 			${body}		
 		</body>
